645294a1b887986b728eb0f0288724c06f732c50
g-bolmida <gbolmida@georgebolmida.com>
2022-10-24 18:26:51 -0400

    sanitize logs

--- src/helpers/logs.go
+++ src/helpers/logs.go
@@ -3,12 +3,23 @@ package helpers
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 func SuccessLog(req *http.Request) {
-	log.Printf("successful %s on %s from %s\n", req.Method, req.RequestURI, req.UserAgent())
+	sanitizedURL := strings.Replace(req.URL.String(), "\n", "", -1)
+	sanitizedURL = strings.Replace(sanitizedURL, "\r", "", -1)
+	sanitizedAgent := strings.Replace(string(req.UserAgent()), "\n", "", -1)
+	sanitizedAgent = strings.Replace(sanitizedAgent, "\r", "", -1)
+
+	log.Printf("successful %s on %s from %s\n", req.Method, sanitizedURL, sanitizedAgent)
 }
 
 func EndpointError(msg string, req *http.Request) {
-	log.Printf("error: %s for %s request on %s from %s", msg, req.Method, req.RequestURI, req.UserAgent())
+	sanitizedURL := strings.Replace(req.URL.String(), "\n", "", -1)
+	sanitizedURL = strings.Replace(sanitizedURL, "\r", "", -1)
+	sanitizedAgent := strings.Replace(string(req.UserAgent()), "\n", "", -1)
+	sanitizedAgent = strings.Replace(sanitizedAgent, "\r", "", -1)
+
+	log.Printf("error: %s for %s request on %s from %s", msg, req.Method, sanitizedURL, sanitizedAgent)
 }
